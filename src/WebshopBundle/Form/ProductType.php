cb93c51022f89044595216e5b0efd43ca0e1023d
daryl <daryl_holling@hotmail.com>
2018-04-08 18:23:58 +0200

    test commit push

---
+++ src/WebshopBundle/Form/ProductType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace WebshopBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProductType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('code')->add('omschrijving')->add('prijs')->add('btw')        ;
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'WebshopBundle\Entity\Product'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'webshopbundle_product';
+    }
+
+
+}
